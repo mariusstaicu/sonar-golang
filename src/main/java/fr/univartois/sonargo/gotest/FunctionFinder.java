3bcf133a62e93d8e6bafbebe55057f1d92d44404
Marius Staicu <marius.staicu@esolutions.ro>
2017-09-21 14:56:56 +0300

    -fixed named finder

--- src/main/java/fr/univartois/sonargo/gotest/FunctionFinder.java
+++ src/main/java/fr/univartois/sonargo/gotest/FunctionFinder.java
@@ -1,15 +1,15 @@
 package fr.univartois.sonargo.gotest;
 
+import org.sonar.api.batch.sensor.SensorContext;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.stream.Stream;
-
-import org.sonar.api.batch.sensor.SensorContext;
-import org.sonar.api.utils.log.Logger;
-import org.sonar.api.utils.log.Loggers;
 
 public class FunctionFinder {
     private static final Logger LOGGER = Loggers.get(FunctionFinder.class);
@@ -19,51 +19,52 @@ public class FunctionFinder {
     private Stream<Path> paths;
 
     public FunctionFinder(SensorContext context) throws IOException {
-	this.baseDir = context.fileSystem().baseDir().getPath();
-	paths = Files.walk(Paths.get(baseDir)).filter(p -> p.toFile().getName().endsWith("_test.go"));
+        this.baseDir = context.fileSystem().baseDir().getPath();
+        paths = Files.walk(Paths.get(baseDir)).filter(p -> p.toFile().getName().endsWith("_test.go"));
     }
 
     public HashMap<String, String> searchFunction() {
 
-	paths.forEach((p) -> {
-	    LOGGER.info("search test function in " + p.toFile().getAbsolutePath());
-	    searchFunctionInFile(p);
-	});
-	LOGGER.debug(result.toString());
-	return result;
+        paths.forEach((p) -> {
+            LOGGER.info("search test function in " + p.toFile().getAbsolutePath());
+            searchFunctionInFile(p);
+        });
+        LOGGER.debug(result.toString());
+        return result;
     }
 
     private void searchFunctionInFile(Path p) {
 
-	try (Stream<String> stream = Files.lines(p)) {
+        try (Stream<String> stream = Files.lines(p)) {
 
-	    stream.forEach((s) -> {
-		String nameFunction = searchInLine(s);
-		if (nameFunction == null)
-		    return;
-		result.put(nameFunction, p.toFile().getAbsolutePath());
-	    });
+            stream
+                    .filter(line -> line.contains("(t *testing.T)"))
+                    .forEach((line) -> {
+                        String nameFunction = getName(line);
+                        if (nameFunction == null)
+                            return;
+                        result.put(nameFunction, p.toFile().getAbsolutePath());
+                    });
 
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public String searchInLine(String s) {
-	int indexTesting = s.indexOf("Test");
-	int indexBrace = s.indexOf('{');
+    public String getName(String line) {
+        int indexTesting = line.indexOf("Test");
 
-	if (indexTesting == -1) {
-	    LOGGER.warn("This function name is not correct: " + s);
-	    return null;
-	}
-	int indexParen = s.indexOf("(", indexTesting);
-	if (indexParen == -1) {
-	    LOGGER.warn("This function name is not correct: " + s);
-	    return null;
-	}
+        if (indexTesting == -1) {
+            LOGGER.warn("This function name is not correct: " + line);
+            return null;
+        }
+        int indexParen = line.indexOf("(", indexTesting);
+        if (indexParen == -1) {
+            LOGGER.warn("This function name is not correct: " + line);
+            return null;
+        }
 
-	return s.substring(indexTesting, indexParen);
+        return line.substring(indexTesting, indexParen);
     }
 
 }
--- src/test/java/fr/univartois/sonargo/FunctionFinderTest.java
+++ src/test/java/fr/univartois/sonargo/FunctionFinderTest.java
@@ -3,14 +3,14 @@
  */
 package fr.univartois.sonargo;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException;
-
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import fr.univartois.sonargo.gotest.FunctionFinder;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author thibault
@@ -27,9 +27,9 @@ public class FunctionFinderTest extends AbstractSonarTest {
     public void testSearchInFile() {
 	try {
 	    FunctionFinder f = new FunctionFinder(testerContext);
-	    assertEquals("TestAverage", f.searchInLine("func TestAverage(t *testing.T)"));
-	    assertEquals("TestGutterBalls", f.searchInLine("func TestGutterBalls(t *testing.T)"));
-	    assertEquals("TestOnePinOnEveryThrow", f.searchInLine("func TestOnePinOnEveryThrow(t *testing.T) {"));
+	    assertEquals("TestAverage", f.getName("func TestAverage(t *testing.T)"));
+	    assertEquals("TestGutterBalls", f.getName("func TestGutterBalls(t *testing.T)"));
+	    assertEquals("TestOnePinOnEveryThrow", f.getName("func TestOnePinOnEveryThrow(t *testing.T) {"));
 	} catch (IOException e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
